f1d83e0fd61240aab4f1c5f30a06b095e8928684
Cyril Chotard <cchotard@placedestendances.com>
2025-08-07 22:59:54 +0200

    feature (Demand): Add Doctrine persistance

--- api/src/Entity/Signature.php
+++ api/src/Entity/Signature.php
@@ -3,21 +3,27 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
 
 /** A signature of a demand. */
+#[ORM\Entity]
 #[ApiResource]
 class Signature
 {
     /** The ID of this signature. */
+    #[ORM\Id, ORM\Column, ORM\GeneratedValue]
     private ?int $id = null;
 
     /** The author of the signature. */
+    #[ORM\Column]
     public string $author = '';
 
     /** The date of publication of this signature.*/
+    #[ORM\Column]
     public ?\DateTimeImmutable $publicationDate = null;
 
     /** The demand this signature is about. */
+    #[ORM\ManyToOne(inversedBy: 'signatures')]
     public ?Demand $demand = null;
 
     public function getId(): ?int
