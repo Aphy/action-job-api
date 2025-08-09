d97fa32ee23a1ce6b5f0faa523626a1ba90b7b12
Cyril Chotard <cchotard@placedestendances.com>
2025-08-09 23:22:14 +0200

    feature (Demand): fix e2e tests

--- e2e/tests/example.spec.js
+++ e2e/tests/example.spec.js
@@ -9,20 +9,20 @@ test('homepage', async ({ page }) => {
 test('swagger', async ({ page }) => {
   await page.goto('https://localhost/docs');
   await expect(page).toHaveTitle('Hello API Platform - API Platform');
-  await expect(page.locator('.operation-tag-content > span')).toHaveCount(5);
+  await expect(page.locator('.operation-tag-content > span')).toHaveCount(10);
 });
 
 test('admin', async ({ page, browserName }) => {
   await page.goto('https://localhost/admin');
   await page.getByLabel('Create').click();
-  await page.getByLabel('Name').fill('foo' + browserName);
+  await page.getByLabel('Title').fill('foo' + browserName);
   await page.getByLabel('Save').click();
   await expect(page).toHaveURL(/admin#\/greetings$/);
   await page.getByText('foo' + browserName).first().click();
   await expect(page).toHaveURL(/show$/);
   await page.getByLabel('Edit').first().click();
-  await page.getByLabel('Name').fill('bar' + browserName);
+  await page.getByLabel('Title').fill('bar' + browserName);
   await page.getByLabel('Save').click();
-  await expect(page).toHaveURL(/admin#\/greetings$/);
+  await expect(page).toHaveURL(/admin#\/demands$/);
   await page.getByText('bar' + browserName).first().click();
 });
